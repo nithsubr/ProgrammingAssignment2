03fe28ca2735b602ae1c64fa73e5584b56af78e7
nithsubr <nithsubr@cisco.com>
2015-09-25 16:15:18 +0530

    Matrix_Inverse_Compute
    Functions to compute the inverse of a matrix. Either the inverse is
    calculated / obtained from cache is already calculated and stored in
    cache memory,

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below funtions take a matrix as an argument and compute the inverse of that matrix
+## Results is either cacluated or obtains the from cache if found
 
-## Write a short comment describing this function
+# The makeCacheMatrix function creates a special matrix with the below sub funtions enabled -
+# 1. setmat = Sets the values to defalts
+# 2. getmat = returns the matrix
+# 3. setinverse = caches the inverse of the matrix
+# 4. getinverse = returns the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  # Initialize
+  m <- matrix(data = NaN, nrow = 1, ncol = 1)
+  
+  # Define setmat
+  setmat <- function(y) {
+    x <<- y
+    m <<- matrix(data = NaN, nrow = 1, ncol = 1)
+  }
+  
+  # Define getmat
+  getmat <- function()
+    x
+  
+  # Define setinverse
+  setinverse <- function(inv_mtr)
+    m <<- inv_mtr
+  
+  # Define getinverse
+  getinverse <- function()
+    m
+  
+  # Return the list of above defined functions
+  list(
+    setmat = setmat, getmat = getmat,
+    setinverse = setinverse,
+    getinverse = getinverse
+    )
+  
 }
 
 
-## Write a short comment describing this function
+# The cacheSolve funtion calculates and returnd the inverse of a matrix
+# it either cacluates the inverse or obtains the same from cache if found
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  # Try to get the inverse from Cache
+  m <- x$getinverse()
+  
+  # If the matrix is obtained from cache, print a confirmation message
+  # and return the inverse matrix
+  if (!is.nan(m[1]))
+  {
+    message("getting cached data")
+    return(m)
+  }
+  
+  # If the reverse matrix is not found in Cache, calculate the same using solve()
+  data <- x$getmat()
+  m <- solve(data)
+  
+  # Update cache with the above calculated inverse matrix
+  x$setinverse(m)
+  
+  # Return the inverse matrix
+  m
+  
 }
